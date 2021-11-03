d9e0e620b6f6f2174220234eceb63c35c4a4bc43
moneetor <moneetor@mon.net.pl>
2021-11-03 12:20:02 +0100

    DirStats - project start

---
+++ cmake-build-debug/CMakeFiles/bcf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bcf.dir/dirstats/dirstats.cpp.o"
+  "bcf"
+  "bcf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bcf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bcf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bcf.
+# This may be replaced when dependencies are built.
